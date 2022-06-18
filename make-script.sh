2d77a20a95ed7996b798f51967546dd59ae38f1d
Joshuad2uiuc <joshuad2@illinois.edu>
2022-06-18 19:10:48 -0400

    first commit

---
+++ make-script.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# usage: bash make-script.sh "Name of essay"
+# params : The actual plain English title of the essay (e.g. Leaves of Grass)
+
+lowercase_title=$(echo "$@" | tr '[:upper:]' '[:lower:]')
+
+# convert title to hyphenate
+hyphenated_title=$(echo "$lowercase_title" | tr " " "-")
+
+# create directory
+cp -R ~/writing/default-template $hyphenated_title
+
+# Rename main to hyphenated title
+mv ~/writing/$hyphenated_title/main.tex ~/writing/$hyphenated_title/$hyphenated_title.tex 
+
+# Insert title as latex file name
+sed -i '' -e "s/\title{Title}/\title{$@}/g" ~/writing/$hyphenated_title/$hyphenated_title.tex 
+
+# Grab date
+date=$(date '+%B %Y')
+
+# Put date into file 
+sed -i '' -e "s/\date{Date}/\date{$date}/g" ~/writing/$hyphenated_title/$hyphenated_title.tex 
