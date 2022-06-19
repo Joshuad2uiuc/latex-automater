0e790c4cfa2c6a2420847b3e3aa9da26c38ed447
Joshuad2uiuc <joshuad2@illinois.edu>
2022-06-18 20:02:31 -0400

    add config and make script reusable

--- make-script.sh
+++ make-script.sh
@@ -1,28 +1,31 @@
 #!/bin/bash
 # usage: bash make-script.sh "Name of essay"
 # params : The actual plain English title of the essay (e.g. Leaves of Grass)
-
-author="FILL ME OUT"
+reldir="$( dirname -- "$0"; )";
+author=$( grep 'author:' $reldir/config.yml | tail | cut -d '"' -f 2)
+repo_location=$( grep 'repo_location:' $reldir/config.yml | tail | cut -d : -f 2)
 
 lowercase_title=$(echo "$@" | tr '[:upper:]' '[:lower:]')
 
 # convert title to hyphenate
 hyphenated_title=$(echo "$lowercase_title" | tr " " "-")
 
+title=$(echo "$@" | xargs)
+
 # create directory
-cp -R ~/writing/default-template $hyphenated_title
+cp -R $repo_location/default-template $hyphenated_title
 
 # Rename main to hyphenated title
-mv ~/writing/$hyphenated_title/main.tex ~/writing/$hyphenated_title/$hyphenated_title.tex 
+mv ./$hyphenated_title/main.tex ./$hyphenated_title/$hyphenated_title.tex
 
 # Insert title as latex file name
-sed -i '' -e "s/\title{Title}/\title{$@}/g" ~/writing/$hyphenated_title/$hyphenated_title.tex 
+sed -i '' -e "s/\title{Title}/\title{$title}/g" ./$hyphenated_title/$hyphenated_title.tex 
 
 # Insert author name in file
-sed -i '' -e "s/\title{Title}/\title{$author}/g" ~/writing/$hyphenated_title/$hyphenated_title.tex 
+sed -i '' -e "s/\author{Author}/\author{$author}/g" ./$hyphenated_title/$hyphenated_title.tex 
 
 # Grab date
 date=$(date '+%B %Y')
 
 # Put date into file 
-sed -i '' -e "s/\date{Date}/\date{$date}/g" ~/writing/$hyphenated_title/$hyphenated_title.tex 
+sed -i '' -e "s/\date{Date}/\date{$date}/g" ./$hyphenated_title/$hyphenated_title.tex 
